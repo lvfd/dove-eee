f0cfb0a7bdfd571ab8e23c48e6747097b621b65b
lvfd <lvfd@acca.com.cn>
2023-04-12 09:28:24 +0800

    process

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path')
 const express = require('express')
 const ejs = require('ejs')
 const multer = require('multer')
+const session = require('express-session')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 const env = process.env.NODE_ENV? process.env.NODE_ENV: 'production'
@@ -29,8 +31,34 @@ app.use(`/${appname}/libs`, express.static(path.resolve(dirname, 'libs')))
 app.use(`/${appname}/release`, express.static(path.resolve(dirname, 'release')))
 app.use(`/${appname}/uploads`, express.static(path.resolve(dirname, 'uploads')))
 
+app.use(session({
+  name: 'dove.eee.uid',
+  secret: 'goose',
+  resave: false,
+  saveUninitialized: false,
+  rolling: true,
+  cookie: {}
+}))
+
+/* Login Page: */
 app.get(`/${appname}`, (req, res) => res.render('login'))
-app.get(`/${appname}/main`, (req, res) => res.render('main'))
+
+/* Login Post: */
+app.post(`/${appname}/login`, bodyParser.urlencoded({ extended: false }), (req, res) => {
+  const loginInfo = req.body
+  const session = req.session
+  for ( var p in loginInfo ) {
+    session[p] = loginInfo[p]
+  }
+  res.redirect(`/${appname}/main`)
+})
+
+/* Main Page: */
+app.get(`/${appname}/main`, (req, res) => {
+  // res.render('main')
+  res.send(req.session)
+})
+
 app.get(`/${appname}/edit`, (req, res) => res.render('edit'))
 
 app.post(`/${appname}/images/upload`, upload.single('file'), (req, res) => {
--- src/login.js
+++ src/login.js
@@ -1,3 +1,65 @@
 import UIkit from 'uikit'
 import Icons from 'uikit/dist/js/uikit-icons'
+import axios from 'axios'
+
 UIkit.use(Icons)
+const dangerClass = 'uk-form-danger'
+const successClass = 'uk-form-success'
+const dataServer = 'http://localhost:8080'
+window.addEventListener('DOMContentLoaded', () => {
+  const inputForUserName = document.querySelector('#username')
+  const inputsForSubmit = document.querySelectorAll('.uk-card-body input')
+  const buttonForLogin = document.querySelector('#loginBtn')
+  inputForUserName.addEventListener('blur', checkUserName)
+  inputsForSubmit.forEach(input => input.addEventListener('focus', e => e.target.classList.remove(successClass, dangerClass)))
+  buttonForLogin.addEventListener('click', loginFunction)
+})
+function checkUserName(event) {
+  return new Promise((resolve, reject) => {
+    const input = event? event.target: document.querySelector('#username')
+    const userName = input.value.trim()
+    if (!userName) {
+      input.classList.add(dangerClass)
+      UIkit.modal.alert('用户名不能为空')
+      return resolve(false)
+    }
+    axios.post(`${dataServer}/dove-eee-data/checkUserName`, { name: userName })
+    .then(response => {
+      if (response.data === true) {
+        input.classList.add(successClass)
+        return resolve(true)
+      }
+      input.classList.add(dangerClass)
+      UIkit.modal.alert('用户不存在或被禁用')
+      return resolve(false)
+    })
+    .catch(error => {
+      console.error(error)
+      input.classList.add(dangerClass)
+      UIkit.modal.alert(error)
+      return reject()
+    })
+  })
+}
+async function loginFunction() {
+  const userNamePass = await checkUserName()
+  if (userNamePass === false) return
+  const passwordInput = document.querySelector('#password')
+  let postData = {
+    username: document.querySelector('#username').value.trim(),
+    password: passwordInput.value.trim()
+  }
+  axios.post(`${dataServer}/dove-eee-data/checkPassword`, postData)
+  .then(response => {
+    if (response.data === true) {
+      passwordInput.classList.add(successClass)
+      return document.querySelector('#loginForm').submit()
+    }
+    passwordInput.classList.add(dangerClass)
+    UIkit.modal.alert('密码错误')
+  })
+  .catch(error => {
+    console.error(error)
+    UIkit.modal.alert(error)
+  })
+}
